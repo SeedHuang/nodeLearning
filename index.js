da24a4168efcc772c9cda137b53d1baf9919d943
huangchunhua <huangchunhua@bilibili.com>
2024-07-10 17:32:45 +0800

    feat: update my express

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = express();
 app.listen(3000);
 
 app.get('/favicon.ico', (req, res)=>{
-    res.writeHead(200);
-    res.end();
+    res.status(200);
     return;
 });
 let winCount = 0;
@@ -15,43 +14,54 @@ let winCount = 0;
 app.get('/reset', (req, res) => {
     winCount = 0;
     console.log('winCount has been reset');
-    res.end('winCount has been reset');
+    res.send('winCount has been reset');
 });
 
-app.get('/game', (req, res) => {
-    const action = req.query.action || '';
-    console.log(`Action is ${action}`);
-    if(winCount > 5) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end(`I won't play`);
-        return;
-    }
-    res.writeHead(200);
-    if(action !== '') {
-        switch(action) {
-            case 'jiandao':
-                console.log('成功');
-                winCount++;
-                res.end('成功');
-                break;
-            case 'shitou':
-                console.log('失败');
-                winCount++;
-                res.end('失败');
-                break;
-            case 'bu':
-                console.log('平局');
-                winCount++;
-                res.end('平局');
-                break;
+app.get('/game', 
+    (req, res, next) => {
+        const action = req.query.action || '';
+        console.log(`Action is ${action}`);
+        if(winCount > 5) {
+            // res.writeHead(500, {'Content-Type': 'text/plain'});
+            // res.end(`I won't play`);
+            res.status(500);
+            res.send(`I won't play`);
+            return;
         }
-    } else {
-        console.log('You have no action');
-        res.end();
+        // res.status(200); //一般情况下不写res.status，默认就是200
+        res.action = action;
+        next();
+    }, 
+    function(req, res){
+        console.log('In next');
+        const action = res.action;
+        console.log('action is ', action);
+        if(action !== '') {
+            switch(action) {
+                case 'jiandao':
+                    console.log('成功');
+                    winCount++;
+                    res.send('成功');
+                    break;
+                case 'shitou':
+                    console.log('失败');
+                    winCount++;
+                    res.send('失败');
+                    break;
+                case 'bu':
+                    console.log('平局');
+                    winCount++;
+                    res.send('平局');
+                    break;
+            }
+        } else {
+            console.log('You have no action');
+            res.end();
+        }
     }
-});
+);
 
 app.get('/', (req, res)=>{
-    res.writeHead(200);
-    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
+    res.send(fs.readFileSync(`${__dirname}/index.html`, 'utf-8'));
+    // fs.createReadStream(`${__dirname}/index.html`).pipe(res);
 });
